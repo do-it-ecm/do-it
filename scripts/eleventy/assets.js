776f3e80b18e6348843cfbc2ae4a9550f957b409
BoxBoxJason <loickgoupil@yahoo.fr>
2025-01-02 20:49:05 +0100

    Add build time rewrite of the media paths for prod Based on @lauravietor https://github.com/FrancoisBrucker/do-it/commit/842af8512966ee683e2ecb0595660eaa815c4810 Rewrite URLs when the env is set to prod Bring back the passthrough for media when env is not set to prod

--- scripts/eleventy/assets.js
+++ scripts/eleventy/assets.js
@@ -4,8 +4,13 @@
 
 export default function (eleventyConfig) {
   const PASS_THROUGH_GLOBS = [
+    "src/**/*.{jpg,png,ico,pdf,svg,gif,webp}",
+    "src/**/*.{txt,edi,csv,json,pdf,zip}",
+    "src/**/*.{webm,mov,mp4,ogv}",
   ];
-  PASS_THROUGH_GLOBS.forEach((glob) => {
-    eleventyConfig.addPassthroughCopy(glob);
-  });
+  if (process.env.NODE_ENV !== "production") {
+    PASS_THROUGH_GLOBS.forEach((glob) => {
+      eleventyConfig.addPassthroughCopy(glob);
+    });
+  }
 };
