264326b19dd636efaa518bdecc47e855c232051a
BoxBoxJason <contact@boxboxjason.dev>
2025-01-08 23:13:09 +0100

    Remove legacy npm run build-tailwind (It is already included in the post build of eleventy)

--- scripts/eleventy/post-build.js
+++ scripts/eleventy/post-build.js
@@ -7,13 +7,13 @@ import { execSync } from "child_process";
 export default function (eleventyConfig) {
 
     eleventyConfig.on('eleventy.after', () => {
-        execSync(`tailwindcss -i ./src/assets/stylesheets/main.css -o ./dist/assets/stylesheets/main.css`)
+        execSync(`tailwindcss -i ./src/assets/stylesheets/main.css -o ./dist/assets/stylesheets/main.css --minify`)
     })
 
     // Search config with pagefind
     eleventyConfig.on('eleventy.after', () => {
         execSync(
-            `npx pagefind --site dist --output-subdir _pagefind --exclude-selectors "img, video, audio, svg"`,
+            `npx pagefind --site dist --output-subdir _pagefind --exclude-selectors "img, video, audio, svg, source"`,
             { encoding: 'utf-8' })
     })
 }
