6e260d48b87dadebfa9ec5f6ce83de49335b3f0c
BoxBoxJason <loickgoupil@yahoo.fr>
2025-01-15 15:26:18 +0100

    Tailwind styling fix

--- scripts/eleventy/post-build.js
+++ scripts/eleventy/post-build.js
@@ -7,13 +7,16 @@ import { execSync } from "child_process";
 export default function (eleventyConfig) {
 
     eleventyConfig.on('eleventy.after', () => {
-        execSync(`tailwindcss -i ./src/assets/stylesheets/main.css -o ./dist/assets/stylesheets/main.css --minify`)
-    })
+        console.log("Post build TailwindCSS");
+        execSync(`tailwindcss -i ./src/assets/stylesheets/main.css -o ./dist/assets/stylesheets/main.css --minify`);
+    });
 
     // Search config with pagefind
     eleventyConfig.on('eleventy.after', () => {
+        console.log("Post build pagefind");
         execSync(
             `npx pagefind --site dist --output-subdir _pagefind --exclude-selectors "img, video, audio, svg, source"`,
-            { encoding: 'utf-8' })
-    })
+            { encoding: 'utf-8' });
+        console.log("Pagefind done");
+    });
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ import typography from '@tailwindcss/typography';
 export default {
     mode: 'jit',
     content: [
-        "./src/assets/stylesheets/*.{html,js,njk}",
-        "./src/*.{html,js,njk}",
-        "./src/_includes/**/*.{html,js,njk}",
-        "./src/cs/**/*.{html,js,njk}",
-        "./src/mon/**/*.{html,js,njk}",
-        "./src/pok/**/*.{html,js,njk}",
-        "./src/projets/**/*.{html,js,njk}",
-        "./scripts/eleventy/markdown/shortcodes/paired/!(index).js",
-        "./scripts/eleventy/markdown/shortcodes/single/!(index).js"
+        "./dist/**/*.{html,css,js}",
     ],
     theme: {
         extend: {
