5373335d48590a6f13de83158409d2ddaaea4c1b
BoxBoxJason <loickgoupil@yahoo.fr>
2025-01-17 22:40:30 +0100

    Update footer Add shortcode for currentyear (might not be required) Change the message for a more stylish one

--- scripts/eleventy/markdown/shortcodes/single/index.js
+++ scripts/eleventy/markdown/shortcodes/single/index.js
@@ -5,10 +5,12 @@
 import setupSizedImage from './sizedImage.js'
 import setupLienInterne from "./lieninterne.js";
 import setupTitle from "./title.js";
+import setupCurrentYear from "./currentyear.js";
 
 
 export default function (eleventyConfig) {
     setupSizedImage(eleventyConfig);
     setupLienInterne(eleventyConfig);
     setupTitle(eleventyConfig);
+    setupCurrentYear(eleventyConfig);
 }
