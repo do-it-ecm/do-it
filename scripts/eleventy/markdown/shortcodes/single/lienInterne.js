444fb2ec6441a745a7ba6c85b9ee927cbd5c0a5f
Valentin Billa <valentin.billa@gmail.com>
2025-03-10 01:23:09 +0100

    Renaming some tag to be camelCase
    - lieninterne -> lienInterne
    - currentyear -> currentYear
    
    Also fixed a bug in lieninterne that did not support correctly fragments

--- scripts/eleventy/markdown/shortcodes/single/lienInterne.js
+++ scripts/eleventy/markdown/shortcodes/single/lienInterne.js
@@ -2,31 +2,29 @@ import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
 
-    eleventyConfig.addShortcode('lieninterne', function (url, dateFormat = "dd/MM/yyyy") {
+    eleventyConfig.addShortcode('lienInterne', function (url, dateFormat = "dd/MM/yyyy") {
 
-        let resolvedUrl = url.startsWith("/")
-            ? url
-            : url.startsWith("./")
-                ? this.page.url + url.substring(2, this.page.url.lastIndexOf("/") + 1)
-                : url;
-
-        if (!resolvedUrl.endsWith("/")) {
-            resolvedUrl += "/";
-        }
+        const resolvedUrl = url.trim()
+            // If the url starts with './' (or unique '.') we replace them with the base url to make an absolute one.
+            .replace(/^\.\/|^\.$/, this.page.url)
+            // Removes fragment from url so that a link can target a specific part of the internal article.
+            .split('#')[0]
+            // Adds a trailing backslash if it is missing because collection's urls all have one.
+            .replace(/(?<!\/)$/, "/");
 
         const article = this.ctx.collections.all.find(item => item.url.endsWith(resolvedUrl));
 
         const title = article?.data.title || "Titre indisponible";
         const résumé = article ?
-            article.data.résumé || "Résumé non disponible"
-            : `Résumé non disponible pour le contenu à l'adresse '${url}'`;
-        const authors = article?.data.authors || "Auteur(s) inconnu(s)";
-        const date = article ? DateTime.fromJSDate(article?.date).toFormat(dateFormat) : "Date non disponible";
+            article.data.résumé || "Résumé indisponible"
+            : `Impossible de trouver l'article à l'adresse '${resolvedUrl}'`;
+        const authors = article?.data?.authors || "Auteur(s) inconnu(s)";
+        const date = article?.date ? DateTime.fromJSDate(article.date).toFormat(dateFormat) : "Date indisponible";
 
         return `
             <div class="m-3 p-3 border-solid border-2 rounded relative flex flex-col">
                 <div class="text-lg mb-3">
-                    <a href="${resolvedUrl}">${title}</a>
+                    <a href="${url}">${title}</a>
                 </div>
                 <span class="line-clamp-3">${résumé}</span>
                 <div class="flex flex-wrap flex-row grow justify-between items-end not-prose list-none mt-3 mb-1 mx-0 px-1 gap-1">
