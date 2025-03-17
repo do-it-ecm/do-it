672551556d530f8466658596b08d44bfa3ad9f23
BoxBoxJason <contact@boxboxjason.dev>
2025-03-17 23:11:42 +0100

    Add timers to build

--- scripts/eleventy/post-build.js
+++ scripts/eleventy/post-build.js
@@ -16,17 +16,19 @@ export default function (eleventyConfig) {
         // Search config with pagefind
         eleventyConfig.on('eleventy.after', () => {
             console.log("Post build pagefind");
+            let start = Date.now();
             execSync(
                 `npx pagefind --site dist --output-subdir _pagefind --exclude-selectors "img, video, audio, svg, source"`,
                 { encoding: 'utf-8' });
-            console.log("Pagefind done");
+            console.log(`⏱️  Pagefind done in ${(Date.now() - start) / 1000}s ⏱️`);
         });
 
         // Add gzip compression
         eleventyConfig.on('eleventy.after', () => {
             console.log("Post build gzip");
+            let start = Date.now();
             execSync(`find ./dist -type f \\( -name "*.html" -o -name "*.css" -o -name "*.js" \\) -exec gzip -k -9 {} \\;`);
-            console.log("Gzip done");
+            console.log(`⏱️  Gzip done in ${(Date.now() - start) / 1000}s ⏱️`);
         });
     }
 }
