4a15832c13fca8d54c18d750512ec4d0c0c93fc1
BoxBoxJason <contact@boxboxjason.dev>
2025-03-17 23:53:52 +0100

    build: upgrade container security
    Run processes as non root user
    Add auto apk update & upgrade

--- scripts/deployment/entrypoint.sh
+++ scripts/deployment/entrypoint.sh
@@ -7,10 +7,10 @@ if [ -z "$GITHUB_TOKEN" ]; then
 fi
 
 # Clone the repository
-git clone --branch gh-pages --single-branch https://github.com/do-it-ecm/do-it.git /opt/do-it/do-it
+su do-it -c "git clone --branch gh-pages --single-branch https://github.com/do-it-ecm/do-it.git /opt/do-it/do-it"
 
 # Start the python server (listening to POST requests on port 3001)
-python3 /opt/do-it/jobs/server.py --update-script /opt/do-it/jobs/synchronizer.sh --github-repo-owner do-it-ecm --github-repo do-it --github-branch gh-pages --secret-token ${GITHUB_TOKEN} --log-file /opt/do-it/logs/server.log --host localhost --port 3001 &
+su do-it -c "python3 /opt/do-it/jobs/server.py --update-script /opt/do-it/jobs/synchronizer.sh --github-repo-owner do-it-ecm --github-repo do-it --github-branch gh-pages --secret-token ${GITHUB_TOKEN} --log-file /opt/do-it/logs/server.log --host localhost --port 3001" &
 
 # Start the nginx server
 exec nginx -g "daemon off;"
