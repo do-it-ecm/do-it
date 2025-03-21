290c31a4dbabebf235074116c6b69e3809abea01
BoxBoxJason <contact@boxboxjason.dev>
2025-03-21 11:59:39 +0100

    chore: add SEO referencing to website
    Add meta tags to all pages
    Add an auto built sitemap
    Optimize build templates

--- scripts/eleventy/assets.js
+++ scripts/eleventy/assets.js
@@ -4,7 +4,7 @@
 
 export default function (eleventyConfig) {
   const PASS_THROUGH_GLOBS = [
-    "src/**/*.{jpg,png,ico,svg,gif,webp}",
+    "src/**/*.{jpg,jpeg,png,ico,svg,gif,webp}",
     "src/**/*.{webm,mov,mp4,ogv}",
   ];
 
